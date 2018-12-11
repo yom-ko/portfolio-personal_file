7d2ef08c9f82fc5042fd23ca0d1a936d198f20fd
yom-ko <artem.getcode@gmail.com>
2018-12-11 21:44:35 +0300

    Multiple important fixes

--- app/js/main.js
+++ app/js/main.js
@@ -1,24 +1,26 @@
 const arrow = document.querySelector('.arrow');
 const clickZone = document.querySelector('.info-js__clickzone');
 
+// Add event listener to the clickable zone just above the scale
 clickZone.addEventListener(
   'click',
   (ev) => {
     const rect = clickZone.getBoundingClientRect();
-    const xCoord = Math.round(ev.clientX - rect.left);
+    const clickCoordX = Math.round(ev.clientX - rect.left);
+    let x;
 
-    let x = 0;
-
-    if (xCoord <= 75) {
+    // Determine in which half of each interval a click happened
+    if (clickCoordX <= 75) {
       x = -10;
-    } else if (xCoord > 75 && xCoord <= 264) {
+    } else if (clickCoordX > 75 && clickCoordX <= 264) {
       x = 139;
-    } else if (xCoord > 264 && xCoord <= 575) {
+    } else if (clickCoordX > 264 && clickCoordX <= 575) {
       x = 367;
     } else {
       x = 755;
     }
 
+    // Move arrow pointer
     arrow.style.left = x;
   },
   false,
