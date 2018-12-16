0aaf841a030c89aba4dd6bf47a29150db21f5a75
yom-ko <artem.getcode@gmail.com>
2018-12-16 21:46:01 +0300

    Migrate to Gulp 4

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,9 @@
 // Load gulp and gulp-load-plugin
 const gulp = require('gulp');
-const imageminWebp = require('imagemin-webp');
 const plugins = require('gulp-load-plugins')();
 
 // Load utility npm modules
 const del = require('del');
-const sequence = require('run-sequence');
 const browsersync = require('browser-sync').create();
 plugins.sass.compiler = require('node-sass');
 
@@ -42,15 +40,18 @@ const paths = {
 /* ----------------- */
 
 // Clean `.tmp` folder
-gulp.task('clean:tmp', () => del.sync(['.tmp/**/*']));
+gulp.task('clean:tmp', (done) => {
+  del.sync(['.tmp/**/*']);
+  done();
+});
 
 // Preprocess and copy HTML files to `.tmp` folder
-gulp.task('prep:pug', () => gulp
+gulp.task('prep:html', () => gulp
   .src(paths.src.html)
   .pipe(plugins.plumber())
   .pipe(plugins.if('*.pug', plugins.pug({ pretty: true })))
   .pipe(gulp.dest(paths.tmp.html))
-  .pipe(browsersync.stream())); // full reload
+  .pipe(browsersync.stream())); // reload
 
 // Preprocess and copy SCSS/CSS files to `.tmp` folder
 gulp.task('prep:styles', () => gulp
@@ -71,101 +72,106 @@ gulp.task('prep:js', () => gulp
     }),
   )
   .pipe(gulp.dest(paths.tmp.js))
-  .pipe(browsersync.stream())); // full reload
+  .pipe(browsersync.stream())); // reload
 
 // Preprocess and copy image files to `.tmp` folder
 gulp.task('prep:images', () => gulp
   .src(paths.src.images)
   .pipe(plugins.plumber())
   .pipe(gulp.dest(paths.tmp.images))
-  .pipe(browsersync.stream())); // inject
+  .pipe(browsersync.stream())); // reload
 
 // Start development server and watch for source changes
-gulp.task('serve', ['prep:pug', 'prep:styles', 'prep:js', 'prep:images'], () => {
-  browsersync.init({
-    host: '192.168.1.10',
-    server: {
-      baseDir: paths.tmp.root,
-    },
-    reloadOnRestart: true,
-    reloadDelay: 1000,
-    notify: false,
-    open: false,
-  });
+gulp.task(
+  'serve',
+  gulp.series('prep:html', 'prep:styles', 'prep:js', 'prep:images', () => {
+    browsersync.init({
+      host: '192.168.1.10',
+      server: {
+        baseDir: paths.tmp.root,
+      },
+      reloadOnRestart: true,
+      reloadDelay: 1000,
+      notify: false,
+      open: false,
+    });
 
-  // Watch for changed and new source HTML files
-  plugins.watch(paths.src.html, () => {
-    gulp.start('prep:pug');
-  });
+    // Watch for changed and new source HTML files
+    plugins.watch(paths.src.html, () => {
+      gulp.start('prep:html');
+    });
 
-  // Watch for changed and new source SCSS/CSS files
-  plugins.watch(paths.src.styles, () => {
-    gulp.start('prep:styles');
-  });
+    // Watch for changed and new source SCSS/CSS files
+    plugins.watch(paths.src.styles, () => {
+      gulp.start('prep:styles');
+    });
 
-  // Watch for changed and new source JS files
-  plugins.watch(paths.src.js, () => {
-    gulp.start('prep:js');
-  });
+    // Watch for changed and new source JS files
+    plugins.watch(paths.src.js, () => {
+      gulp.start('prep:js');
+    });
 
-  // Watch for changed and new source image files
-  plugins.watch(paths.src.images, () => {
-    gulp.start('prep:images');
-  });
-});
+    // Watch for changed and new source image files
+    plugins.watch(paths.src.images, () => {
+      gulp.start('prep:images');
+    });
+  }),
+);
 
 /* ----------------- */
 // Optimization Tasks
 /* ----------------- */
 
 // Clean `dist` folder
-gulp.task('clean:dist', () => del.sync(['dist/**/*']));
+gulp.task('clean:dist', (done) => {
+  del.sync(['dist/**/*']);
+  done();
+});
 
 // Optimize and copy image files from `.tmp` to `dist` folder
-gulp.task('opt:images', ['prep:images'], () => gulp
-  .src(`${paths.tmp.images}/**/*.{png,jpg,jpeg,gif,svg}`)
-  .pipe(plugins.plumber())
-  .pipe(
-    plugins.imagemin([
-      imageminWebp({ quality: 80 }),
-      plugins.imagemin.svgo({
-        plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
-      }),
-    ]),
-  )
-  .pipe(plugins.if('*.jpeg', plugins.extReplace('.webp')))
-  .pipe(plugins.if('*.jpg', plugins.extReplace('.webp')))
-  .pipe(plugins.if('*.png', plugins.extReplace('.webp')))
-  .pipe(gulp.dest(paths.dist.images)));
+gulp.task(
+  'opt:images',
+  gulp.series('prep:images', () => gulp
+    .src(`${paths.tmp.images}/**/*.{jpg,jpeg,png,gif,svg}`)
+    .pipe(plugins.plumber())
+    .pipe(
+      plugins.imagemin([
+        plugins.imagemin.gifsicle({ interlaced: true }),
+        plugins.imagemin.jpegtran({ progressive: true }),
+        plugins.imagemin.optipng({ optimizationLevel: 5 }),
+        plugins.imagemin.svgo({
+          plugins: [{ removeViewBox: true }, { cleanupIDs: false }],
+        }),
+      ]),
+    )
+    .pipe(gulp.dest(paths.dist.images))),
+);
 
 // Optimize and copy HTML, CSS, and JS files from `.tmp` to `dist` folder
-gulp.task('opt:all', ['prep:pug', 'prep:styles', 'prep:js', 'opt:images'], () => gulp
-  .src(`${paths.tmp.html}/*.html`)
-  .pipe(plugins.plumber())
-  .pipe(plugins.useref())
-  .pipe(plugins.if('*.css', plugins.replace(/\.(jpg|jpeg|png|gif)/g, '.webp'))) // Replace image file extensions in urls
-  .pipe(plugins.if('*.css', plugins.csso({ comments: false }))) // Optimize linked CSS files
-  .pipe(plugins.if('*.js', plugins.uglify())) // Optimize linked JS files
-  .pipe(plugins.if('*.html', plugins.replace(/\.(jpg|jpeg|png|gif)/g, '.webp')))
-  .pipe(
-    plugins.if(
-      '*.html',
-      plugins.htmlmin({
-        collapseWhitespace: true,
-        minifyCSS: true,
-      }),
-    ),
-  ) // Optimize HTML files
-  .pipe(gulp.dest(paths.dist.root)));
+gulp.task(
+  'opt:all',
+  gulp.series('prep:html', 'prep:styles', 'prep:js', 'opt:images', () => gulp
+    .src(`${paths.tmp.html}/*.html`)
+    .pipe(plugins.plumber())
+    .pipe(plugins.useref())
+    .pipe(plugins.if('*.css', plugins.csso({ comments: false }))) // Optimize linked CSS files
+    .pipe(plugins.if('*.js', plugins.uglify())) // Optimize linked JS files
+    .pipe(
+      plugins.if(
+        '*.html',
+        plugins.htmlmin({
+          collapseWhitespace: true,
+          minifyCSS: true,
+        }),
+      ),
+    ) // Optimize HTML files
+    .pipe(gulp.dest(paths.dist.root))),
+);
 
 /* ----------------- */
 // Main tasks
 /* ----------------- */
 
-gulp.task('default', () => {
-  sequence('clean:tmp', 'serve');
-});
+gulp.task('default', gulp.series('clean:tmp', 'serve'));
 
-gulp.task('build', () => {
-  sequence('clean:dist', 'opt:all');
-});
+gulp.task('build', gulp.series('clean:dist', 'opt:all'));
